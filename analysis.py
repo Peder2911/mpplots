8538936b33423997d512301024eaa9baba21eabe
peder2911 <pglandsverk@gmail.com>
2020-05-11 16:10:58 +0200

    started work on catplot, mostly finished with regular

--- analysis.py
+++ analysis.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot as plt
 from collections import defaultdict
 import textwrap
 import numpy as np
+import re
 
 Codebook = Dict[str,Dict[str,str]]
 Descriptions = Dict[str,str]
@@ -53,51 +54,79 @@ def getDescriptions(dat: pd.DataFrame) -> Descriptions:
 def getDescriptionsFromExcel(*args,**kwargs):
     return getDescriptions(pd.read_excel(*args,**kwargs))
 
+stripPrefix = lambda x: re.sub("(_FW|_AT)$","",x)
+
 if __name__ == "__main__":
     codebook = getCodebookFromExcel("docs/Codebook_AT_New Dictionary.xlsx",sheet_name="Label")
     desc = getDescriptionsFromExcel("docs/Codebook_AT_New Dictionary.xlsx",sheet_name="Dictionary")
+
     dat = pd.read_csv("raw/mergedFinal.csv",encoding="latin1",low_memory=False)
     dat.columns = [c.upper() for c in dat.columns] 
+    dat.columns = [stripPrefix(c) for c in dat.columns]
+
 
     pvm = pd.read_csv("raw/Plot_variable_mapping - Sheet1.csv")
     histograms = pvm[pvm["Type"] == "Hist"]
 
+    print("Doing:")
+    print("\n".join(histograms["Variable"]))
+
+    print("Results:")
     for idx,r in histograms.iterrows():
+
         r["Variable"] = str(r["Variable"]).upper()
 
         if str(r["Variable"]).lower() == "nan":
             print(f"Missing variable for {r['Slide']}")
             continue
 
-        try:
-            values = dat[r["Variable"]]
-        except KeyError:
-            print(f"{r['Variable']} not in data")
+        
+        # Make subsets
+        pltdat = []
+        for c in ["a","b"]:
+
+            try:
+                values = dat[r["Variable"]]
+            except KeyError:
+                print(f"{r['Variable']} not in data")
+                continue
+
+
+            values = values[np.invert(np.isnan(values))]
+            values = values.apply(lambda x: int(x))
+
+            try:
+                values = values.apply(lambda x: codebook[r['Variable']].get(str(x),"NA"))
+            except KeyError:
+                print(f"{r['Variable']} not in codebook")
+                continue
+
+            counts = values.value_counts()
+            percentages = counts.apply(lambda x: (x / counts.sum())*100) 
+
+            labels = counts.index
+            labels = ["\n".join(textwrap.wrap(l,25)) for l in labels]
+
+            cat = [c]*len(labels)
+            pltdat.append(pd.DataFrame({"values":percentages,"labels":labels,"category":cat}))
+
+        if not pltdat:
             continue
 
-        values = values[np.invert(np.isnan(values))]
-        values = values.apply(lambda x: int(x))
-        values = values.apply(lambda x: codebook[r['Variable']].get(str(x),"NA"))
-
-        counts = values.value_counts()
-        percentages = counts.apply(lambda x: (x / counts.sum())*100) 
-
-        labels = counts.index
-        labels = ["\n".join(textwrap.wrap(l,25)) for l in labels]
+        pltdat = pd.concat(pltdat)
 
         title = "\n".join(textwrap.wrap(desc[r["Variable"]],60))
 
         plt.clf()
+        # Non-grouped plot 
+        #chart = sns.barplot(y=labels,x=percentages,ax = ax)
 
-        fig,ax = plt.subplots()
+        # Grouped plot
+        chart = sns.catplot(y="labels",x="values",hue="category",kind="bar",data = pltdat,height=10,aspect=2.5) 
+        plt.subplots_adjust(top=0.85)
+        chart.fig.suptitle(title)
 
-        chart = sns.barplot(y=labels,x=percentages,ax = ax)
-        chart.set_title(title)
-
-        ax.figure.subplots_adjust(left=0.3)
-        ax.set(xlabel = "%")
-
-        fig.set_size_inches(11.7,8.27)
-
+        #ax.figure.subplots_adjust(left=0.3)
+        chart.set_axis_labels("%","")
         plt.savefig(f"plots/{r['Slide']}_{r['Variable']}.png")
 
