72fe820c3a1aff13fc2d57326bf4fd5fa7b5c89b
peder2911 <pglandsverk@gmail.com>
2020-06-12 15:17:10 +0200

    Codebook STATA code generator

---
+++ codebookToDofile.py
@@ -0,0 +1,43 @@
+
+from util import getCodebookFromExcel
+import pandas as pd
+
+cb = getCodebookFromExcel("docs/cb2.xlsx")
+
+
+frames = []
+for var, valMap in cb.items():
+    keys = []
+    values = []
+
+    for key,val in valMap.items():
+        keys.append(key)
+        values.append('"'+val+'"')
+
+    frames.append(pd.DataFrame({
+        "variable": var,
+        "key": keys,
+        "val": values
+    }))
+
+codebookSheet = pd.concat(frames)
+
+# Making a stata file
+
+codebookSheet["pair"] = codebookSheet["key"] + " " + codebookSheet["val"]
+
+vlines = (codebookSheet[["variable","pair"]]
+    .groupby("variable")
+    .agg(lambda x: " ".join(x))
+    .reset_index()
+)
+
+dolines = []
+
+print("Writing some STATA code...")
+for idx,ln in vlines.iterrows():
+    dolines.append(" ".join(["label define",ln["variable"],ln["pair"]]))
+    dolines.append(" ".join(["label values",ln["variable"],ln["variable"]]))
+with open("dofiles/translate.do","w") as f:
+    f.write("\n".join(dolines))
+print("done!")
